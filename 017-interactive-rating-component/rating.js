d4996f35deb82d183c8ba7a908cabe6eb4e01e24
Silas <silascosta310@gmail.com>
2023-05-09 12:42:54 -0300

    Finalizando botões selecionáveis

--- 017-interactive-rating-component/rating.js
+++ 017-interactive-rating-component/rating.js
@@ -7,15 +7,13 @@ let
 
 
 function selecaoElemento(id){
+    if(botoesSel.length === 1){
+        document.getElementById(botoesSel[0]).classList.remove("selecionado");
+        botoesSel.pop()
+    }
     if (botoesSel.length === 0){ // Botoes selecionados (adiciona apenas se nenhum botão estiver selecionado)
         botoesSel.push(id); // Adiciona a lista de selecionados
         document.getElementById(id).classList.add("selecionado") //Add bg
-    }else{
-        document.getElementById(botoesSel[0]).classList.remove("selecionado");
-        botoesSel.pop()
-        
-
-        
     }
 }
 
