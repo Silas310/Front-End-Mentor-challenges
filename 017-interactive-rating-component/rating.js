c099ccf0358de97abe215dd8669a6031250b5001
Silas <silascosta310@gmail.com>
2023-05-09 12:38:15 -0300

    Adicionando modo selecionado

---
+++ 017-interactive-rating-component/rating.js
@@ -0,0 +1,25 @@
+let 
+    form = document.getElementById("ratingForm"),
+    submit = document.getElementById("submit-btn"),
+    botoesSel = [];
+
+// alert(form.children[i].id)
+
+
+function selecaoElemento(id){
+    if (botoesSel.length === 0){ // Botoes selecionados (adiciona apenas se nenhum botão estiver selecionado)
+        botoesSel.push(id); // Adiciona a lista de selecionados
+        document.getElementById(id).classList.add("selecionado") //Add bg
+    }else{
+        document.getElementById(botoesSel[0]).classList.remove("selecionado");
+        botoesSel.pop()
+        
+
+        
+    }
+}
+
+function avaliadorNota(){
+    console.log("oi")
+        
+}
