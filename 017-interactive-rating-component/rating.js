9b44ebc14893467be7e6a36b6ae92461da144451
Silas <silascosta310@gmail.com>
2023-05-10 15:07:50 -0300

    Corrigindo alguns erros

--- 017-interactive-rating-component/rating.js
+++ 017-interactive-rating-component/rating.js
@@ -1,15 +1,25 @@
 let 
     form = document.getElementById("ratingForm"),
     submit = document.getElementById("submit-btn"),
+    rating = document.getElementById("rating"),
     botoesSel = [];
 
 function selecaoElemento(id){
     if(botoesSel.length === 1){
         document.getElementById(botoesSel[0]).classList.remove("selecionado");
         botoesSel.pop()
-    }
-    if (botoesSel.length === 0){ // Botoes selecionados (adiciona apenas se nenhum botão estiver selecionado)
+    }else if (botoesSel.length === 0){ // Botoes selecionados (adiciona apenas se nenhum botão estiver selecionado)
         botoesSel.push(id); // Adiciona a lista de selecionados
         document.getElementById(id).classList.add("selecionado") //Add bg
     }
 }
+
+
+/*function avaliadorNota(){
+    showMSG(botoesSel[0][4])
+}*/
+
+
+/*function showMSG(nota){
+    rating.innerText = 'tchau'
+}*/
