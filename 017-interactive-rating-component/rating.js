6eb7d59925b9078a5cdc5963e067fcf0b1cf4ad9
Silas <silascosta310@gmail.com>
2023-05-12 15:37:06 -0300

    Completando projeto 017

--- 017-interactive-rating-component/rating.js
+++ 017-interactive-rating-component/rating.js
@@ -2,7 +2,15 @@ let
     form = document.getElementById("ratingForm"),
     submit = document.getElementById("submit-btn"),
     rating = document.getElementById("rating"),
+    main = document.getElementById("main-rating"),
+    thanks = document.getElementById("main-thanks"),
     botoesSel = [];
+
+
+form.addEventListener('submit', event =>{
+    event.preventDefault();
+})
+
 
 function selecaoElemento(id){
     do{
@@ -22,4 +30,16 @@ function selecaoElemento(id){
     while(botoesSel.length !== 1) // Exc o codigo enquanto btn n for selecionado
     /* do-while adiciona a possibilidade de trocar o botão sem ter que
     desmarcar o botão ja selecionado */
+}
+
+
+function avaliadorNota(event){
+    if(botoesSel.length === 0){ // Se valor nao foi selecionado
+        alert("selecione um valor")            
+    }else{
+        main.style.display = "none"
+        /*thanks.classList.add("display")*/
+        thanks.style.display = "flex"
+        rating.innerHTML = `You selected ${botoesSel[0][4]} out of 5`
+    }
 }
