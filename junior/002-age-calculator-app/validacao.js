2fa370f4feab4bbfd8761a972ef75da4c4eb67cc
Silas <silascosta310@gmail.com>
2023-07-04 10:25:02 -0300

    Refazer verificação de validade

--- junior/002-age-calculator-app/validacao.js
+++ junior/002-age-calculator-app/validacao.js
@@ -6,50 +6,10 @@ form[0].addEventListener("submit", event =>{
     event.preventDefault();
 })
 
-for(let i = 0; i < 3; i++){
+/*for(let i = 0; i < 3; i++){
     form[0][i].addEventListener("input", verificacao);
-}
+}*/
 
-function verificacao(event){
-    let id = event.currentTarget.id,
-        elemento = document.getElementById(id),
-        validity = null,
-        index = null;
-    switch(id){
-        case "input-day":
-            if( elemento.value < 0 || 
-                elemento.value > 31){
-                validity = false;
-                index = 0;
-            }else{
-                validity = true;
-                index = 0;
-            }
-            break;
-        case "input-month":
-            if( elemento.value < 0 || 
-                elemento.value> 12){
-                validity = false;
-                index = 1;
-            }else{
-                validity = true;
-                index = 1
-            }
-            break;
-        case "input-year":
-            if( elemento.value > 2023 || 
-                elemento.value < 0){
-                validity = false;
-                index = 2;
-            }else{
-                validity = true;
-                index = 2;
-            }
-            break;
-    }
-    mudarEstado(validity, index);
-
-}
 
 function mudarEstado(validade, index){
     if(validade === true){
@@ -63,5 +23,4 @@ function mudarEstado(validade, index){
         form[0].children[index].children[2].
         classList.remove("displayNone");
     }
-
 }
