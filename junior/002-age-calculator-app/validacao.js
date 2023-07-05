ea7d2e6ebde31b56579d592bd8244fca3021f534
Silas <silascosta310@gmail.com>
2023-07-05 11:44:52 -0300

    Adicionando verificação de validade

--- junior/002-age-calculator-app/validacao.js
+++ junior/002-age-calculator-app/validacao.js
@@ -1,14 +1,49 @@
 let form = document.querySelectorAll("form"),
     labels = document.querySelectorAll("label");
+const data = new Date();
 
 
 form[0].addEventListener("submit", event =>{
     event.preventDefault();
 })
 
-/*for(let i = 0; i < 3; i++){
+for(let i = 0; i < 3; i++){
     form[0][i].addEventListener("input", verificacao);
-}*/
+}
+
+form[0][3].addEventListener("click", enviar);
+
+
+function verificacao(event, id = event.target.id){
+    let diasDisponiveis,
+        elemento = document.getElementById(id),
+        statusAtual = false;
+    console.log(id)
+    switch(id){
+        case "input-day":
+            if( elemento.value < 0 || 
+                elemento.value > 31 || 
+                elemento.value > diasDisponiveis){
+                mudarEstado(false, 0);
+                statusAtual = false
+            }else{ mudarEstado(true, 0); statusAtual = true;}
+            break;
+        case "input-month":
+            if(elemento.value < 0 ||
+                elemento.value > 12){
+                    mudarEstado(false, 1);
+                    statusAtual = false;
+                }else{ mudarEstado(true, 1); statusAtual = true;}
+            break;
+        case "input-year":
+            if(elemento.value < 0 || 
+                elemento.value > 2023){
+                    mudarEstado(false, 2);
+                    statusAtual = false;
+            }else{ mudarEstado(true, 2); statusAtual = true;}
+        break;         
+    }
+}
 
 
 function mudarEstado(validade, index){
@@ -23,4 +58,9 @@ function mudarEstado(validade, index){
         form[0].children[index].children[2].
         classList.remove("displayNone");
     }
+}
+
+
+function enviar(){
+    verificacao("input-day");
 }
