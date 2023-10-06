827826bbc02cc66a2d327b1f3f0396d02f8f2978
Silas <silascosta310@gmail.com>
2023-10-06 17:36:00 -0300

    Menu Mobile
    Corrigindo algumas configurações

--- junior/004-news-homepage/assets/scripts/menu.js
+++ junior/004-news-homepage/assets/scripts/menu.js
@@ -1,9 +1,9 @@
 function openMenu(){
-    let menu = document.querySelector(".menu-items-container");
+    let menu = document.querySelector("#menu-items-container");
     menu.style.display = "flex";
 }
 
 function closeMenu(){
-    let menu = document.querySelector(".menu-items-container");
+    let menu = document.querySelector("#menu-items-container");
     menu.style.display = "none";
 }
