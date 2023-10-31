97d59cc341cd22727f1f84caa9b6bd638cb9f9e1
Silas <silascosta310@gmail.com>
2023-10-31 12:58:47 -0300

    Ajustando menu
    Adicionando opacidade

--- junior/004-news-homepage/assets/scripts/menu.js
+++ junior/004-news-homepage/assets/scripts/menu.js
@@ -1,9 +1,11 @@
 function openMenu(){
     let menu = document.querySelector("#menu-items-container");
     menu.style.display = "flex";
+    document.body.style.backgroundColor = "rgb(0,0,0, 0.4)";
 }
 
 function closeMenu(){
     let menu = document.querySelector("#menu-items-container");
     menu.style.display = "none";
+    document.body.style.backgroundColor = "white";
 }
