a2ea98a40a41f9769f9e6e8db4771e885fcd69e4
Silas <silascosta310@gmail.com>
2024-01-26 15:02:11 -0300

    Header
    Reorganizando código do header, estilos e funcionalidade do meu

--- junior/006-intro-section-with-dropdown-navigation/menu.js
+++ junior/006-intro-section-with-dropdown-navigation/menu.js
@@ -1,39 +1,33 @@
 function controlarMenu() {
   let largura = atualizarLargura(),
-      nav = document.querySelector(".header__nav-list"),
-      auths = document.querySelector(".header__auth-links");
+      nav = document.querySelector(".header__items");
 
   if (largura < 64) {
-    if ( verificarStatusMenu(nav, auths) == false) {
-      abrirMenu(nav, auths);
+    if ( verificarStatusMenu(nav) == false) {
+      abrirMenu(nav);
     } else {
-      fecharMenu(nav, auths);
+      fecharMenu(nav);
     }
   }
 
 }
 
-
-function abrirMenu(nav, auths) {
-  nav.classList.remove("header__nav-list_not-displayed");
-  auths.classList.remove("header__auth-links_not-displayed");
+function abrirMenu(nav) {
+  nav.classList.remove("header__items_not-displayed");
 }
 
-function fecharMenu(nav, auths) {
-  nav.classList.add("header__nav-list_not-displayed");
-  auths.classList.add("header__auth-links_not-displayed");
+function fecharMenu(nav) {
+  nav.classList.add("header__items_not-displayed");
 }
 
 
-function verificarStatusMenu(nav, auths) {
-  if ( ( nav.classList.contains("header__nav-list_not-displayed") ) && 
-  ( auths.classList.contains("header__auth-links_not-displayed") ) ) {
+function verificarStatusMenu(nav) {
+  if ( nav.classList.contains("header__items_not-displayed") ) {
     return false;
   } else {
     return true;
   }
 }
-
 
 function atualizarLargura() {
   let larguraJanela = window.innerWidth;
