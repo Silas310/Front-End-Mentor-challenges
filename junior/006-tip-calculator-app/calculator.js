b3a6ff95918c82bb4fd9120fd722d6fa30a6d514
Silas <silascosta310@gmail.com>
2024-03-16 10:17:13 -0300

    Adicionando target a função de armazenamento de valores

--- junior/006-tip-calculator-app/calculator.js
+++ junior/006-tip-calculator-app/calculator.js
@@ -4,14 +4,14 @@ let customInput = document.querySelector(".tip-input");
 let peopleInput = document.querySelector(".people-input");
 let results = document.querySelectorAll(".result");
 
-function storeValues() {
+function storeValues(e) {
   let values = [];
-  
+  console.log(e.target);
 }
 
 billInput.addEventListener("input", storeValues);
 for (let i = 0; i < tipBtn.length; i++) {
-  let item = tipBtn[i].addEventListener("click", storeValues)
+  let item = tipBtn[i].addEventListener("click", storeValues);
 }
 customInput.addEventListener("input", storeValues);
 peopleInput.addEventListener("input", storeValues);
