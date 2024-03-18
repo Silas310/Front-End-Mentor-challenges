0e659904e461dd9df49ee49701470cda5690972d
Silas <silascosta310@gmail.com>
2024-03-18 12:53:20 -0300

    Criando função de calculo

--- junior/006-tip-calculator-app/calculator.js
+++ junior/006-tip-calculator-app/calculator.js
@@ -4,14 +4,53 @@ let customInput = document.querySelector(".tip-input");
 let peopleInput = document.querySelector(".people-input");
 let results = document.querySelectorAll(".result");
 
-function storeValues(e) {
-  let values = [];
-  console.log(e.target);
+
+function manageValues(e) {
+  switch (e.target.className) {
+    case "input bill-input":
+      storeValues(e.target.value, 1);
+      break;
+    case "btn":
+    case "input tip-input":
+      storeValues(e.target.value, 2);
+      break;
+    case "input people-input":
+      storeValues(e.target.value, 3);
+      break;
+  }
 }
 
-billInput.addEventListener("input", storeValues);
-for (let i = 0; i < tipBtn.length; i++) {
-  let item = tipBtn[i].addEventListener("click", storeValues);
+let values = {};
+function storeValues(value, type) {
+  switch (type) {
+    case 1:
+      values.total = value;
+      break;
+    case 2:
+      values.tip = value;
+      break;
+    case 3:
+      values.divider = value;
+      break;
+  }
+  Object.keys(values).length;
+  calculateTip(values);
 }
-customInput.addEventListener("input", storeValues);
-peopleInput.addEventListener("input", storeValues);
+
+
+function calculateTip(operands) {
+  console.log(operands);
+  if (Object.keys(operands).length == 3) {
+    let total = parseFloat(operands.total),
+        tip = parseFloat(operands.tip),
+        divider = parseFloat(operands.divider);
+    console.log( Math.round((total * tip) / divider) );
+  }
+}
+
+billInput.addEventListener("input", manageValues);
+for (let i = 0; i < tipBtn.length; i++) {
+  let item = tipBtn[i].addEventListener("click", manageValues);
+}
+customInput.addEventListener("input", manageValues);
+peopleInput.addEventListener("input", manageValues);
