3c3f6c31286ec397ef5443900814e20b33121aa3
Silas <silascosta310@gmail.com>
2024-04-02 13:29:28 -0300

    JS
    Finalizando o afuncionalidade Mark All As Read

--- junior/007-notifications-page/notifHandler.js
+++ junior/007-notifications-page/notifHandler.js
@@ -1,3 +1,10 @@
-let notifications = document.
-querySelectorAll(".notification.notification-unread");
-console.log(notifications);
+document.querySelector(".mark-btn").
+addEventListener("click", markAsRead);
+
+function markAsRead() {
+  let notifications = document.
+  querySelectorAll(".notification.notification-unread");
+  for (let i = 0; i < 3; i++) {
+    notifications[i].classList.remove("notification-unread");
+  }
+}
