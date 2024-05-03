ed3a6e4fccf00e734e4ca253c13deea00191aca6
Silas <silascosta310@gmail.com>
2024-05-03 14:48:58 -0300

    Trocando value por nome de classes

--- intermediate/001-calculator-app/calculatorApp.js
+++ intermediate/001-calculator-app/calculatorApp.js
@@ -1,22 +1,2 @@
 document.querySelector(".operators").
 addEventListener("click", handleCalculatorInput);
-
-
-function buildMathExpression() {
-
-}
-
-
-function handleFunction () {
-
-}
-
-
-function handleCalculatorInput(event){
-  let value = event.target.value;
-  if(value.length === 1) {
-    buildMathExpression(value);
-  }else {
-    console.log("função");
-  }
-}
