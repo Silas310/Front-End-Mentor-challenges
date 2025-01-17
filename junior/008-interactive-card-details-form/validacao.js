6e8a60c4e120dc84a489c766c72d81650cc43918
Silas <silascosta310@gmail.com>
2025-01-17 13:12:39 -0300

    Adicionando eventos, criando funções de atualização e scritura

--- junior/008-interactive-card-details-form/validacao.js
+++ junior/008-interactive-card-details-form/validacao.js
@@ -1,3 +1,39 @@
+let form = document.querySelector("form");
 let inputs = document.querySelectorAll("form input");
 let errorMSGs = document.querySelectorAll(".error-msg");
-console.log(inputs, errorMSGs);
+
+
+for (let input of inputs) {
+  if (input.type !== "submit") {  
+    input.addEventListener("input", updateCards);
+    input.addEventListener("blur", validateInput);
+  }
+}
+
+
+function updateCards(event) {
+  const input = event.target;
+  const inputIndex = Array.from(inputs).indexOf(input);
+  
+  writeCard(inputIndex, input.value);
+}
+
+
+function writeCard(index, value) {
+  const cardFields = document.querySelectorAll(".card-field");
+
+
+  switch (index) {
+    case 0:
+      if (cardFields[1]) {
+        cardFields[1].innerHTML = value.trim().toUpperCase();
+      }
+      break;
+
+    case 1:
+      if (cardFields[0]) {
+        cardFields[0].innerHTML = value;
+      }
+      break;
+  }
+}
