cea218e72bb374055003816aa8fa7766f3b5dbc6
Silas <silascosta310@gmail.com>
2025-01-21 11:34:02 -0300

    Criando função de mostrar erro

--- junior/008-interactive-card-details-form/validacao.js
+++ junior/008-interactive-card-details-form/validacao.js
@@ -19,31 +19,24 @@ function validateInput(event) {
     case 0:
       if (isNameBlank(input) && isNameLengthValid(input) ) {
         writeCard(inputIndex, input.value);
-      } else {
-        console.log(inputs[3].value.length);
       }
       break;
+
     case 1:
-      if (isCardNumberValid(input.value)) {
+      if (isCardNumberValid(input)) {
         writeCard(inputIndex, input.value);
-      } else {
-        console.log("ERRO");
       }
       break;
 
     case 2:
       if (isMonthValid(input)) {
         writeCard(inputIndex, input.value);
-      } else {
-        console.log("ERRO");
       }
       break;
 
     case 3:
       if (isYearValid(input)) {
         writeCard(inputIndex, input.value);
-      }else {
-        console.log("ERRO");
       }
       break;
 
@@ -62,7 +55,10 @@ function validateInput(event) {
 
 
 function isNameBlank(input) {
-  if (input.value.trim() == "") { return false;}
+  if (input.value.trim() == "") {
+    showError(input, "Can't be blank");
+    return false;
+  }
   return true;
 }
 
@@ -70,16 +66,18 @@ function isNameBlank(input) {
 function isNameLengthValid(input) {
   let length =  input.value.trim().length;
   if (length > 25 ) {
-    console.log("INVALID")
+    showError(input, "The name must be 25 characters or fewer")
     return false;
   }
   return true;
 }
 
 
-function isCardNumberValid(number) {
+function isCardNumberValid(input) {
+  let number = input.value;
   console.log(number.length)
   if (number.length < 1 || number.length > 16 ) {
+    showError(input, "Only 16 digits allowed");
     return false;
   }
   return true;
@@ -89,7 +87,7 @@ function isCardNumberValid(number) {
 function isMonthValid(input) {
   let month = input.value;
   if (month.length < 1 || month > 12) {
-    console.log("MES ERRO");
+    showError(input, "Invalid month. Please enter a value between 01 and 12")
     return false;
   }
   return true;
@@ -99,9 +97,11 @@ function isMonthValid(input) {
 function isYearValid(input) {
   let year = input.value;
   if (year.length == 2 && year < 25) {
+    showError(input, "Invalid year. Must be 25 or higher");
     return false;
   }
   if (year.length >= 3) {
+    showError(input, "Invalid year");
     return false;
   }
   return true;
@@ -112,12 +112,44 @@ function isCvcValid(input) {
   if (input.value.length < 4) {
     return true;
   }
+  showError(input, "Invalid format");
   return false;
 }
 
 
 function showError(input, msg) {
-  console.log(input, msg);
+  let id =  input.id
+  console.log(input);
+
+  switch (id) {
+    case "cardholder-name":
+      input.classList.add("input-error");
+      errorMSGs[0].classList.remove("display-none");
+      errorMSGs[0].innerHTML = msg;
+      break;
+  
+    case "card-number":
+      input.classList.add("input-error");
+      errorMSGs[1].classList.remove("display-none");
+      errorMSGs[1].innerHTML = msg;
+      break;
+    case "exp-month":
+      input.classList.add("input-error");
+      errorMSGs[2].classList.remove("display-none");
+      errorMSGs[2].innerHTML = msg;
+      break;
+    case "exp-year":
+      input.classList.add("input-error");
+      errorMSGs[2].classList.remove("display-none");
+      errorMSGs[2].innerHTML = msg;
+      break;
+    case "cvc-number":
+      input.classList.add("input-error");
+      errorMSGs[3].classList.remove("display-none");
+      errorMSGs[3].innerHTML = msg;
+      break;
+
+  }
 }
 
 
