f177a37354370721368e1d0d95ad0551eac9ae12
Silas <silascosta310@gmail.com>
2025-01-24 19:32:10 -0300

    Finalizando validação de formulario

--- junior/008-interactive-card-details-form/validacao.js
+++ junior/008-interactive-card-details-form/validacao.js
@@ -10,24 +10,53 @@ for (let input of inputs) {
 }
 
 
+form.addEventListener('submit', event =>{
+  event.preventDefault();
+})
+
+
+inputs[5].addEventListener("click", validateForm);
+
+
 inputs[1].addEventListener("input", (event) => {
   const input = event.target;
   
   let value = input.value.replace(/\D/g, '');
-
+  
   value = value.replace(/(\d{4})(?=\d)/g, '$1 ');
-
+  
   input.value = value;
-
+  
   validateInput(event);
 });
+
+
+function validateForm(){
+  let isValid = true;
+
+  inputs.forEach((input) => {
+    validateInput({ target: input }); // Reutiliza a função existente
+    if (input.classList.contains("input-error")) isValid = false;
+  });
+
+  if (isCardNumberValid(inputs[1]) == false) {
+    showError(inputs[1], "16 digits only");
+    isValid = false;
+  }
+  
+  if (isValid) {
+    document.querySelector(".form-container").classList.add("form-container-complete-state");
+    form.classList.add("display-none");
+    document.querySelector(".confirmation-container").classList.remove("display-none");
+  }
+}
 
 
 function validateInput(event) {
   const input = event.target;
   const inputIndex = Array.from(inputs).indexOf(input);
-
-
+  
+  
   switch (inputIndex) {
     case 0:
       if (isNotBlank(input) && isNameLengthValid(input) && isNameValid(input) ) {
@@ -35,28 +64,28 @@ function validateInput(event) {
         writeCard(inputIndex, input.value);
       }
       break;
-
+      
     case 1:
       if (true) {
         hideError(input);
         writeCard(inputIndex, input.value);
       }
       break;
-
+        
     case 2:
       if (isMonthValid(input)) {
         hideError(input);
         writeCard(inputIndex, input.value);
       }
       break;
-
+          
     case 3:
       if (isYearValid(input)) {
         hideError(input);
         writeCard(inputIndex, input.value);
       }
       break;
-
+      
     case 4:
       if (isCvcValid(input)) {
         hideError(input);
@@ -64,12 +93,12 @@ function validateInput(event) {
       }
       break;
   }
-
-
-  // writeCard(inputIndex, input.value);
+      
+      
+      // writeCard(inputIndex, input.value);
 }
-
-
+    
+    
 function isNotBlank(input) {
   if (input.value.trim() == "") {
     showError(input, "Can't be blank");
@@ -96,6 +125,14 @@ function isNameLengthValid(input) {
     return false;
   }
   return true;
+}
+
+
+function isCardNumberValid(input) {
+  let length = input.value.length;
+  if (length !== 19) {
+    return false;
+  }
 }
 
 
@@ -135,24 +172,25 @@ function isCvcValid(input) {
 
 function showError(input, msg) {
   let id =  input.id; 
-
+  
   switch (id) {
     case "cardholder-name":
       input.classList.add("input-error");
       errorMSGs[0].classList.remove("display-none");
       errorMSGs[0].innerHTML = msg;
       break;
-  
-    case "card-number":
-      input.classList.add("input-error");
-      errorMSGs[1].classList.remove("display-none");
-      errorMSGs[1].innerHTML = msg;
-      break;
-    case "exp-month":
-      input.classList.add("input-error");
-      errorMSGs[2].classList.remove("display-none");
-      errorMSGs[2].innerHTML = msg;
-      break;
+      
+      case "card-number":
+        input.classList.add("input-error");
+        errorMSGs[1].classList.remove("display-none");
+        errorMSGs[1].innerHTML = msg;
+        break;
+        case "exp-month":
+          input.classList.add("input-error");
+          errorMSGs[2].classList.remove("display-none");
+          errorMSGs[2].innerHTML = msg;
+          break;
+          function validateForm(){}
     case "exp-year":
       input.classList.add("input-error");
       errorMSGs[2].classList.remove("display-none");
