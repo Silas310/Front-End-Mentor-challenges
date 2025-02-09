42bb24916be3368866610d3e861451d0293f36ae
Silas <silascosta310@gmail.com>
2025-02-09 15:59:22 -0300

    fix: ajusta breakpoints na geração de imagens

--- junior/009-product-list-with-cart/code.js
+++ junior/009-product-list-with-cart/code.js
@@ -26,10 +26,13 @@ function renderDesserts() {
     div.classList.add("dessert__item");
     
     div.innerHTML = `
-      <div class="desserts__image-container">
+      <picture class="desserts__image-container">
+        <source srcset="${item.image.mobile}" media="(min-width: 23.4375em)">
+        <source srcset="${item.image.tablet}" media="(min-width: 35em) and (max-width: 90em)">
+        <source srcset="${item.image.desktop}" media="(min-width: 90em)">
         <img src="${item.image.thumbnail}" alt="${item.name}" class="desserts__image">
+      </picture>
         <button class="desserts__button">Add to Cart</button>
-      </div>
       <div class="desserts__info">
         <p class="desserts__category">${item.category}</p>
         <h3 class="desserts__title">${item.name}</h3>
