b14b5563c4d6315f6b7eba9c5a4d7328370ac785
Silas <silascosta310@gmail.com>
2025-02-09 15:41:40 -0300

    feat: add elementos na tela
    Função renderDesserts é chamada após fetch e carrega os conteúdos

--- junior/009-product-list-with-cart/code.js
+++ junior/009-product-list-with-cart/code.js
@@ -1,5 +1,6 @@
 let storedData = []; 
 
+
 async function loadData() {
   try {
     let response = await fetch("data.json");
@@ -9,4 +10,33 @@ async function loadData() {
   }
 }
 
-loadData();
+
+loadData().then( () => {
+  renderDesserts();
+});
+
+
+function renderDesserts() {
+  const container = document.querySelector(".desserts");
+  
+
+  storedData.forEach(item => {
+    let div = document.createElement("div");
+
+    div.classList.add("dessert__item");
+    
+    div.innerHTML = `
+      <div class="desserts__image-container">
+        <img src="${item.image.thumbnail}" alt="${item.name}" class="desserts__image">
+        <button class="desserts__button">Add to Cart</button>
+      </div>
+      <div class="desserts__info">
+        <p class="desserts__category">${item.category}</p>
+        <h3 class="desserts__title">${item.name}</h3>
+        <p class="desserts__price">$${item.price.toFixed(2)}</p>
+      </div>
+    `;
+
+    container.appendChild(div);
+  });
+}
