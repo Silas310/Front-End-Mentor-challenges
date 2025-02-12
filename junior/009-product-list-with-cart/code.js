570b3f7188a6b2cd1efbec2bd380f99cae384f36
Silas <silascosta310@gmail.com>
2025-02-11 21:52:00 -0300

    refactor: remove div desnecessária
    Remove div extra

--- junior/009-product-list-with-cart/code.js
+++ junior/009-product-list-with-cart/code.js
@@ -26,19 +26,17 @@ function renderDesserts() {
     div.classList.add("desserts__item");
     
     div.innerHTML = `
-      <div class="desserts__item">
-        <picture class="desserts__image-container">
-          <source srcset="${item.image.mobile}" media="(min-width: 23.4375em)">
-          <source srcset="${item.image.tablet}" media="(min-width: 35em) and (max-width: 90em)">
-          <source srcset="${item.image.desktop}" media="(min-width: 90em)">
-          <img src="${item.image.thumbnail}" alt="${item.name}" class="desserts__image">
-        </picture>
-        <button class="desserts__button">Add to Cart</button>
-        <div class="desserts__info">
-          <p class="desserts__category">${item.category}</p>
-          <h3 class="desserts__title">${item.name}</h3>
-          <p class="desserts__price">$${item.price.toFixed(2)}</p>
-        </div>
+      <picture class="desserts__image-container">
+        <source srcset="${item.image.mobile}" media="(min-width: 23.4375em)">
+        <source srcset="${item.image.tablet}" media="(min-width: 35em) and (max-width: 90em)">
+        <source srcset="${item.image.desktop}" media="(min-width: 90em)">
+        <img src="${item.image.thumbnail}" alt="${item.name}" class="desserts__image">
+      </picture>
+      <button class="desserts__button">Add to Cart</button>
+      <div class="desserts__info">
+        <p class="desserts__category">${item.category}</p>
+        <h3 class="desserts__title">${item.name}</h3>
+        <p class="desserts__price">$${item.price.toFixed(2)}</p>
       </div>
     `;
 
