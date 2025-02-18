4e04517babae3bba407ad8abfe6290a5ed1b7451
Silas <silascosta310@gmail.com>
2025-02-18 19:38:12 -0300

    feat: add função de incremento e decremento

--- junior/009-product-list-with-cart/code.js
+++ junior/009-product-list-with-cart/code.js
@@ -33,12 +33,12 @@ function renderDesserts() {
         <source srcset="${item.image.desktop}" media="(min-width: 90em)">
         <img src="${item.image.thumbnail}" alt="${item.name}" class="desserts__image">
       </picture>
-      <button class="desserts__button desserts__button--add">Add to Cart</button>
+      <button class="desserts__button--add desserts__button">Add to Cart</button>
 
       <button class="desserts__button desserts__button--hidden desserts__button--counter">
-      <img src="assets/images/icon-decrement-quantity.svg" alt="Decrement icon">
-      <div>1</div>
-      <img src="assets/images/icon-increment-quantity.svg" alt="Increment icon">
+      <img src="assets/images/icon-decrement-quantity.svg" alt="Decrement icon" class="desserts__button--decrement">
+      <div class="desserts__counter">1</div>
+      <img src="assets/images/icon-increment-quantity.svg" alt="Increment icon" class="desserts__button--increment">
       </button>
 
       <div class="desserts__info">
@@ -63,18 +63,40 @@ function addButtonListeners() {
 function manageButtons(event) {
   let button = event.target;
   let classList = button.classList;
-  if (classList[1] == "desserts__button--add") {
-    changeButtonClass(button, "adding");
+  let counter = document.querySelector(".desserts__counter");
+  switch (classList[0]) {
+    case "desserts__button--add":
+      changeButtonClass(button, "adding");
+      break;
+
+    case "desserts__button--increment":
+      counter.innerHTML = Number(counter.innerHTML) + 1;
+      break;
+
+    case "desserts__button--decrement":
+      if (Number(counter.innerHTML) > 1) {
+        counter.innerHTML = Number(counter.innerHTML) - 1;
+      } else {
+        changeButtonClass(button, "removing");
+      }
+      break;
   }
 }
 
 
 function changeButtonClass(button, caso) {
-  let nextElement = button.nextElementSibling;
   switch (caso) {
     case "adding":
+      let nextElement = button.nextElementSibling;
       button.classList.add("desserts__button--hidden");
       nextElement.classList.remove("desserts__button--hidden");
       break;
-  }
+    case "removing":
+      let counterBtn = button.parentElement; // Counter btn
+      let addBtn = button.parentElement.previousElementSibling; // Add to cart btn
+
+      addBtn.classList.remove("desserts__button--hidden");
+      counterBtn.classList.add("desserts__button--hidden")
+      break;
+  } 
 }
