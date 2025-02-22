ffe15af19ce7a893acd44b7fee09c64408ca0362
Silas <silascosta310@gmail.com>
2025-02-21 21:01:19 -0300

    feat: add estado de botões
    Adiciona efeito hover nos botões de decremento e incremento

--- junior/009-product-list-with-cart/code.js
+++ junior/009-product-list-with-cart/code.js
@@ -36,9 +36,20 @@ function renderDesserts() {
       <button class="desserts__button--add desserts__button">Add to Cart</button>
 
       <button class="desserts__button desserts__button--hidden desserts__button--counter">
-      <svg class="desserts__button--decrement" xmlns="http://www.w3.org/2000/svg" width="10" height="2" fill="none" viewBox="0 0 10 2"><path fill="#fff" d="M0 .375h10v1.25H0V.375Z"/></svg>
+
+      <div class="desserts__button-container">
+        <svg class="desserts__button--decrement" xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="none" viewBox="0 0 10 2">
+          <path class="path" fill="#fff" d="M0 .375h10v1.25H0V.375Z"/>
+        </svg>
+      </div>
+
       <div class="desserts__counter">1</div>
-      <svg class="desserts__button--increment" xmlns="http://www.w3.org/2000/svg" width="10" height="10" fill="none" viewBox="0 0 10 10"><path fill="#fff" d="M10 4.375H5.625V0h-1.25v4.375H0v1.25h4.375V10h1.25V5.625H10v-1.25Z"/></svg>
+
+      <div class="desserts__button-container">
+        <svg class="desserts__button--increment" xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="none" viewBox="0 0 10 10">
+          <path class="path" fill="#fff" d="M10 4.375H5.625V0h-1.25v4.375H0v1.25h4.375V10h1.25V5.625H10v-1.25Z"/>
+        </svg>
+      </div>
       </button>
 
       <div class="desserts__info">
@@ -112,6 +123,7 @@ function changeButtonClass(button, caso) {
 function selectImage(img) {
   img.classList.add("desserts__image--selected");
 }
+
 
 function deselectImage(img) {
   img.classList.remove("desserts__image--selected");
